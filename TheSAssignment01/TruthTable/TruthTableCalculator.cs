75719c672049c756b3cb6329efe483654d966af9
perawatt <pekung4433@perfenterprise.com>
2020-07-29 19:48:22 +0700

    implement GetTruthTableAndResult test

--- TheSAssignment01/TruthTable/TruthTableCalculator.cs
+++ TheSAssignment01/TruthTable/TruthTableCalculator.cs
@@ -9,19 +9,23 @@ namespace TruthTable
         public IEnumerable<IEnumerable<string>> GetTruthTable(IEnumerable<string> stringSet)
         {
             List<List<string>> truthtable = new List<List<string>> { };
-            var pow = Convert.ToInt32(Math.Pow(stringSet.Count(),2));
+            var pow = Convert.ToInt32(Math.Pow(2, stringSet.Count()));
             var loopRound = pow == 1 ? 2 : pow;
             for (int i = loopRound - 1; i >= 0; i--)
             {
                 List<string> currentRow = new List<string> { };
                 string binary = Convert.ToString(i, 2);
-                if(binary.Length == 1)
+                if (binary.Length != stringSet.Count())
                 {
-                    binary = binary.Insert(0, "0");
+                    var loop = stringSet.Count() - binary.Length;
+                    for (int x = 0; x < loop; x++)
+                    {
+                        binary = binary.Insert(0, "0");
+                    }
                 }
                 foreach (char ch in binary.ToCharArray())
                 {
-                    if(ch == '0')
+                    if (ch == '0')
                     {
                         currentRow.Add("F");
                     }
@@ -33,6 +37,65 @@ namespace TruthTable
                 truthtable.Add(currentRow);
             }
             return truthtable;
+        }
+
+        public IEnumerable<IEnumerable<string>> GetTruthTableAndResult(string operation, IEnumerable<string> stringSet)
+        {
+            List<List<string>> truthtable = new List<List<string>> { };
+            var pow = Convert.ToInt32(Math.Pow(2, stringSet.Count()));
+            var loopRound = pow == 1 ? 2 : pow;
+            for (int i = loopRound - 1; i >= 0; i--)
+            {
+                List<string> currentRow = new List<string> { };
+                string binary = Convert.ToString(i, 2);
+                if (binary.Length != stringSet.Count())
+                {
+                    var loop = stringSet.Count() - binary.Length;
+                    for (int x = 0; x < loop; x++)
+                    {
+                        binary = binary.Insert(0, "0");
+                    }
+                }
+                foreach (char ch in binary.ToCharArray())
+                {
+                    if (ch == '0')
+                    {
+                        currentRow.Add("F");
+                    }
+                    else
+                    {
+                        currentRow.Add("T");
+                    }
+                }
+                currentRow.Add(GetResult(operation, currentRow));
+                truthtable.Add(currentRow);
+            }
+            return truthtable;
+        }
+
+        private string GetResult(string operation, IEnumerable<string> truthtable)
+        {
+            var result = "F";
+            switch (operation)
+            {
+                case "and":
+                    result = truthtable.Contains("F") ? "F" : "T";
+                    break;
+                case "or":
+                    result = truthtable.Contains("T") ? "T" : "F";
+                    break;
+                case "xor":
+                    if (truthtable.Contains("T"))
+                    {
+                        var numberOfTrue = truthtable.Where((it) => it.Contains("T")).Count();
+                        result = numberOfTrue % 2 == 0 ? "F": "T";
+                    }
+                    break;
+                default:
+                    result = "F";
+                    break;
+            }
+            return result;
         }
     }
 }
--- TheSAssignment01/XUnitTest/TruthTableCalculatorTest.cs
+++ TheSAssignment01/XUnitTest/TruthTableCalculatorTest.cs
@@ -10,79 +10,121 @@ namespace XUnitTest
     public class TruthTableCalculatorTest
     {
 
-        public static IEnumerable<object[]> generateTruthTable = new List<object[]>
-        {
-
-            //new object[]
-            //{
-            //    new List<string>{"a"},
-            //    new List<List<string>>
-            //    {
-            //        new List<string> {"T"},
-            //        new List<string> {"F"},
-
-            //    }
-            //},
-            //new object[]
-            //{
-            //    new List<string>{"a","b"},
-            //    new List<List<string>>
-            //    {
-            //        new List<string> {"T","T"},
-            //        new List<string> {"T","F"},
-            //        new List<string> {"F","T"},
-            //        new List<string> {"F","F"}
-
-            //    }
-            //},
-            new object[]
-            {
-                new List<string>{"a","b","c"},
-                new List<List<string>>
-                {
-                    new List<string> {"T","T","T"},
-                    new List<string> {"T","T","F"},
-                    new List<string> {"T","F","T"},
-                    new List<string> {"T","F","F"},
-                    new List<string> {"F","T","T"},
-                    new List<string> {"F","T","F"},
-                    new List<string> {"F","F","T"},
-                    new List<string> {"F","F","F"},
-                }
-            }
-        };
-        [MemberData(nameof(generateTruthTable))]
-        [Theory]
-        public void CalculateTruthTable(IEnumerable<string> stringSet, IEnumerable<IEnumerable<string>> expected)
-        {
-            var sut = new TruthTable.TruthTableCalculator();
-            var actual = sut.GetTruthTable(stringSet);
-            Assert.Equal(expected, actual);
-        }
-
-        //public static IEnumerable<object[]> posibilityTruthTable = new List<object[]>
+        //public static IEnumerable<object[]> generateTruthTable = new List<object[]>
         //{
         //    new object[]
         //    {
-        //        "and",
-        //        new List<string>{"a,b"},
+        //        new List<string>{"a"},
+        //        new List<List<string>>
+        //        {
+        //            new List<string> {"T"},
+        //            new List<string> {"F"},
+
+        //        }
+        //    },
+        //    new object[]
+        //    {
+        //        new List<string>{"a","b"},
+        //        new List<List<string>>
+        //        {
+        //            new List<string> {"T","T"},
+        //            new List<string> {"T","F"},
+        //            new List<string> {"F","T"},
+        //            new List<string> {"F","F"}
+
+        //        }
+        //    },
+        //    new object[]
+        //    {
+        //        new List<string>{"a","b","c"},
         //        new List<List<string>>
         //        {
         //            new List<string> {"T","T","T"},
+        //            new List<string> {"T","T","F"},
+        //            new List<string> {"T","F","T"},
         //            new List<string> {"T","F","F"},
+        //            new List<string> {"F","T","T"},
         //            new List<string> {"F","T","F"},
-        //            new List<string> {"F","F","F"}
-
+        //            new List<string> {"F","F","T"},
+        //            new List<string> {"F","F","F"},
         //        }
         //    }
         //};
-        //[MemberData(nameof(posibilityTruthTable))]
+
+        //[MemberData(nameof(generateTruthTable))]
         //[Theory]
-        //public void CalculateAndOperation(string operationstring, IEnumerable<string> stringSet, IEnumerable<IEnumerable<string>> expected)
+        //public void CalculateTruthTable(IEnumerable<string> stringSet, IEnumerable<IEnumerable<string>> expected)
         //{
         //    var sut = new TruthTable.TruthTableCalculator();
-        //    var actual = sut.GetTruthTable(operationstring,stringSet.ToArray());
-        //    Assert.Equal(expected, actual);
+        //    var actual = sut.GetTruthTable(stringSet);
+        //    actual.Should().BeEquivalentTo(expected);
         //}
+
+        public static IEnumerable<object[]> posibilityTruthTable = new List<object[]>
+        {
+            new object[]
+            {
+                "and",
+                new List<string>{"a","b"},
+                new List<List<string>>
+                {
+                    new List<string> {"T","T","T"},
+                    new List<string> {"T","F","F"},
+                    new List<string> {"F","T","F"},
+                    new List<string> {"F","F","F"}
+
+                }
+            },
+            new object[]
+            {
+                "or",
+                new List<string>{"a","b"},
+                new List<List<string>>
+                {
+                    new List<string> {"T","T","T"},
+                    new List<string> {"T","F","T"},
+                    new List<string> {"F","T","T"},
+                    new List<string> {"F","F","F"}
+
+                }
+            },
+            new object[]
+            {
+                "xor",
+                new List<string>{"a","b"},
+                new List<List<string>>
+                {
+                    new List<string> {"T","T","F"},
+                    new List<string> {"T","F","T"},
+                    new List<string> {"F","T","T"},
+                    new List<string> {"F","F","F"}
+
+                }
+            },
+            //new object[]
+            //{
+            //    "xor",
+            //    new List<string>{"a","b","c"},
+            //    new List<List<string>>
+            //    {
+            //        new List<string> {"T","T","T","T"},
+            //        new List<string> {"T","T","F","F"},
+            //        new List<string> {"T","F","T","F"},
+            //        new List<string> {"T","F","F","T"},
+            //        new List<string> {"F","T","T","F"},
+            //        new List<string> {"F","T","F","T"},
+            //        new List<string> {"F","F","T","T"},
+            //        new List<string> {"F","F","F","F"},
+            //    }
+            //}
+        };
+        [MemberData(nameof(posibilityTruthTable))]
+        [Theory]
+        public void CalculateAndOperation(string operationstring, IEnumerable<string> stringSet, IEnumerable<IEnumerable<string>> expected)
+        {
+            var sut = new TruthTable.TruthTableCalculator();
+            var actual = sut.GetTruthTableAndResult(operationstring, stringSet);
+            actual.Should().BeEquivalentTo(expected);
+        }
     }
 }
