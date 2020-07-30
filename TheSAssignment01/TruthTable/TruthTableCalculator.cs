b11fe3186acbfe2ee7d301c34732619d8796a2a3
perawatt <pekung4433@perfenterprise.com>
2020-07-30 14:06:52 +0700

    refactor

--- TheSAssignment01/TruthTable/TruthTableCalculator.cs
+++ TheSAssignment01/TruthTable/TruthTableCalculator.cs
@@ -6,47 +6,27 @@ namespace TruthTable
 {
     public class TruthTableCalculator
     {
-        public IEnumerable<IEnumerable<string>> GetTruthTable(IEnumerable<string> stringSet)
-        {
-            List<List<string>> truthtable = new List<List<string>> { };
-            var pow = Convert.ToInt32(Math.Pow(2, stringSet.Count()));
-            var loopRound = pow == 1 ? 2 : pow;
-            for (int i = loopRound - 1; i >= 0; i--)
-            {
-                List<string> currentRow = new List<string> { };
-                string binary = Convert.ToString(i, 2);
-                if (binary.Length != stringSet.Count())
-                {
-                    var loop = stringSet.Count() - binary.Length;
-                    for (int x = 0; x < loop; x++)
-                    {
-                        binary = binary.Insert(0, "0");
-                    }
-                }
-                foreach (char ch in binary.ToCharArray())
-                {
-                    if (ch == '0')
-                    {
-                        currentRow.Add("F");
-                    }
-                    else
-                    {
-                        currentRow.Add("T");
-                    }
-                }
-                truthtable.Add(currentRow);
-            }
-            return truthtable;
-        }
-
         public IEnumerable<IEnumerable<string>> GetTruthTableAndResult(string operation, IEnumerable<string> stringSet)
         {
-            List<List<string>> truthtable = new List<List<string>> { };
+            var truthtable = GetTruthTable(stringSet);
+            var truthtableAndResult = new List<List<string>> { };
+            foreach (var item in truthtable)
+            {
+                List<string> rowAndResult = item.ToList();
+                rowAndResult.Add(GetResult(operation, item));
+                truthtableAndResult.Add(rowAndResult);
+            }
+            return truthtableAndResult;
+        }
+
+        public IEnumerable<IEnumerable<string>> GetTruthTable(IEnumerable<string> stringSet)
+        {
+            var truthtable = new List<List<string>> { };
             var pow = Convert.ToInt32(Math.Pow(2, stringSet.Count()));
             var loopRound = pow == 1 ? 2 : pow;
             for (int i = loopRound - 1; i >= 0; i--)
             {
-                List<string> currentRow = new List<string> { };
+                var currentRow = new List<string> { };
                 string binary = Convert.ToString(i, 2);
                 if (binary.Length != stringSet.Count())
                 {
@@ -67,7 +47,6 @@ namespace TruthTable
                         currentRow.Add("T");
                     }
                 }
-                currentRow.Add(GetResult(operation, currentRow));
                 truthtable.Add(currentRow);
             }
             return truthtable;
@@ -88,11 +67,10 @@ namespace TruthTable
                     if (truthtable.Contains("T"))
                     {
                         var numberOfTrue = truthtable.Where((it) => it.Contains("T")).Count();
-                        result = numberOfTrue % 2 == 0 ? "F": "T";
+                        result = numberOfTrue % 2 == 0 ? "F" : "T";
                     }
                     break;
                 default:
-                    result = "F";
                     break;
             }
             return result;
